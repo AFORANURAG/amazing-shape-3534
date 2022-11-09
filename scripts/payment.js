e2262f6c3a4599fda84ada2b4a8e7755cea3097c
AFORANURAG <anuragupadhyay172912313@gmail.com>
2022-11-09 23:27:48 +0530

    day-2 completed navbar completed and elite page started

---
+++ scripts/payment.js
@@ -0,0 +1,3 @@
+import  navbar  from "../components/navbar.js";
+document.getElementById("navbarid").innerHTML=navbar()
+// we append nodes basically and elements and ids , those are html , so they are not working good
