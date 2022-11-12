86e61884a39ee6223071d0d57c0f7bb75a500bb4
Jaydip97 <pjaydip460@gmail.com>
2022-11-12 08:30:28 +0530

    day-5

---
+++ components/footer.js
@@ -0,0 +1,30 @@
+function footer(){
+    return `<div>
+    <h2>Products</h2>
+    <a href="https://www.jefit.com/elite"><p>Elite Membership</p></a>
+    <a href="https://www.jefit.com/coach"><p>Coach</p></a>
+    <a href="https://www.jefit.com/signup"><p>Signup</p></a>
+    <a href="Login"><p>Login</p></a>
+   </div>
+   <div>
+    <h2>Resource</h2>
+    <a href="https://www.jefit.com/routines/"><p>Workout Plans</p></a>
+    <a href="https://www.jefit.com/exercises"><p>Exercises Database</p></a>
+    <a href="https://www.jefit.com/help-center"><p>Help Center</p></a>
+   </div>
+   <div>
+    <h2>Legal Press</h2>
+    <a href="https://www.jefit.com/elite"><p>Privacy Policy</p></a>
+    <a href="https://www.jefit.com/coach"><p>Terms of Use</p></a>
+    <a href="https://www.jefit.com/signup"><p>IP / DMCA Notices</p></a>
+    <a href="Login"><p>Press & Media</p></a>
+   </div>
+   <div>
+    <h2>Follow Us</h2>
+    <a href="https://www.jefit.com/blog"><p>Blog</p></a>
+    <a href="https://www.facebook.com/jefitapp"><p>Facebook</p></a>
+    <a href="https://twitter.com/jefitinc"><p>Twitter</p></a>
+    <a href="https://www.instagram.com/jefitapp/"><p>Instagram</p></a>
+   </div>`
+}
+export default footer;
---
+++ src/footer.js
@@ -0,0 +1,3 @@
+import footer from "../components/footer.js"
+let div_footer=document.getElementById("footer");
+div_footer.innerHTML=footer()
