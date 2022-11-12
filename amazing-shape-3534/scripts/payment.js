5fa9a0e7f59454faf25300fdc94816a94904786b
Jaydip97 <pjaydip460@gmail.com>
2022-11-12 23:25:59 +0530

    ev5

--- amazing-shape-3534/scripts/payment.js
+++
@@ -1,3 +0,0 @@
-import  navbar  from "../components/navbar.js";
-document.getElementById("navbarid").innerHTML=navbar()
-// we append nodes basically and elements and ids , those are html , so they are not working good
