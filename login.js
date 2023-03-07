e86440038df4ce1c3f6b4df87a4b20037449d0a6
AFORANURAG <anuragupadhyay172912313@gmail.com>
2023-03-07 16:07:53 +0530

    project fixed

--- login.js
+++ login.js
@@ -1,89 +1,22 @@
+let loginbtn = document.getElementById("Login").addEventListener("click",handleLogin);
 
-class User {
-    constructor() {
-
-    }
-
-    async Login(u, p) {
-
-        this.username = u
-        this.password = p;
-
-
-        const login_api = `https://masai-api-mocker.herokuapp.com/auth/login`;
-
-        let response = await fetch(login_api, {
-            method: 'POST',
-            body: JSON.stringify(this),
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-
-        const loginData = await response.json();
-        console.log('logdata', loginData)
-
-        if (loginData.error === true) {
-            alert('please enter valid username and password')
-        }
-        else {
-            alert('Login success  you are redirecting to home page')
-            return loginData;
-        }
-    }
-}
-
-let user_loginData = new User();
-
-let login = document.getElementById('Login');
-login.onclick = () => {
-    Login()
-}
-
-const Login = async () => {
-
-    let Pwd = document.getElementById('password').value;
-    let Username = document.getElementById('username').value;
-
-    if (Username == "") {
-        alert('please enter your username')
-    }
-    else if (Pwd == "") {
-        alert('enter your password')
-    }
-
-    else {
-        let { token } = await user_loginData.Login(Username, Pwd);
-        // localStorage.setItem("Jefit_username", Username);
-        getProfile(Username, token)
-    }
-
-}
-
-const getProfile = async (Username, token) => {
-
-    const login_api = `https://masai-api-mocker.herokuapp.com/user/${Username}`;
-
-    let response = await fetch(login_api, {
-        headers: {
-            Authorization: `Bearer ${token}`,
-            'Content-Type': 'application/json'
-        }
-    });
-
-    let profileData = await response.json();
-
-    console.log('profiledata', profileData)
-
-    if (profileData.username !== undefined) {
-        localStorage.setItem("Jefit_username", Username);
-        window.location.href = "index.html";
-
-        // let showname = document.getElementById('showname');
-        // showname.innerText = profileData.username;
-        // showname.style.color = "red"
-    }
-
+function handleLogin(){
+let name=document.getElementById("username").value;
+let email=document.getElementById("email").value;
+let password=document.getElementById("password").value;
+let obj = {name,email,password};
+console.log(obj);
+fetch("https://rich-erin-hippo-tie.cyclic.app/userauth/login",{
+method:"POST" , 
+body:JSON.stringify(obj), 
+  headers:{
+    "Content-Type":"application/json"
+  }
+}).then((data)=>data.json()).then((res)=>{
+localStorage.setItem("Jefit_username", res.document.name);
+console.log(res)
+window.location.href = "index.html";    
+}).catch((err)=>console.log(err));
 }
 
 // registered ids
